3fe969436c0bd31583b49c1587128b453487f5d1
sunwei-ksdy-robot <sunweijm.sun@gmail.com>
2025-12-03 16:12:30 +0800

    add MID360_pointcloud_to_laserscan_launch.py

---
+++ launch/MID360_pointcloud_to_laserscan_launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+
+        Node(
+            package='pointcloud_to_laserscan', 
+            executable='pointcloud_to_laserscan_node',
+            remappings=[('cloud_in', '/livox/lidar/pointcloud'),
+                        ('scan', '/livox/lidar/scan')],
+            parameters=[{
+                'target_frame': 'livox_frame',
+                'transform_tolerance': 0.01,
+                'min_height': 0.0,
+                'max_height': 1.0,
+                'angle_min': -3.1416,  # -M_PI
+                'angle_max': 1.309,  # 75 degree
+                'angle_increment': 0.0087,  # M_PI/360.0
+                'scan_time': 0.3333,
+                'range_min': 0.10,
+                'range_max': 10.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+            name='pointcloud_to_laserscan'
+        )
+    ])
